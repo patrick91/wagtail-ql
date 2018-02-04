0c366a3c9ab2117250cf6566e4d43b6f4526fc82
Patrick Arminio <patrick.arminio@gmail.com>
2018-02-04 19:54:19 +0000

    Add example without custom converter

--- backend/home/schema.py
+++ backend/home/schema.py
@@ -1,8 +1,18 @@
+from graphene.types.generic import GenericScalar
+
 from graphene_django import DjangoObjectType
 
 from .models import SitePage
 
 
 class Page(DjangoObjectType):
+    body_json = GenericScalar(required=True)
+
     class Meta:
         model = SitePage
+
+    def resolve_body(self, info):
+        return self.body
+
+    def resolve_body_json(self, info):
+        return self.body.stream_data
